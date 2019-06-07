7c496308a3a699e14d0be104a5e7e8654ea1d989
Max Gerber <mgerber94@gmail.com>
2019-06-07 12:07:02 +0100

    + Moved content from slide.php to article-intro.php to correspond to new designs  + Added new fields in article-intro.php  +  Working carousel
    Relates #13 #5

--- wp-content/themes/norfolk-counts/partials/article-intro.php
+++ wp-content/themes/norfolk-counts/partials/article-intro.php
@@ -20,15 +20,54 @@ $fields = get_fields($post->ID); ?>
   <div class="row">
     <div class="small-12 medium-6 columns">
       <div class="article-content">
-
         <div class="for-slide">
           <span class="tag"><?= $post->post_title; ?></span>
-          <span class="title"><?= $fields['callout'];?></span>
+
+          <div class="lead-counter">
+            <?= $fields['stats_section']; ?>
+          </div>
+
+          <div class="share-buttons">
+            <?php wp_reset_postdata(); ?>
+            <a href="<?=mailto_link();?>" class="btn-social email" data-log-ga
+            data-ga-category="email-button"
+            data-ga-action="click"
+            data-ga-label="Email"
+            data-fb-event="CompleteAction"
+            data-fb-event-name="Email"
+            data-link-out="<?=mailto_link();?>"><i></i> Email</a>
+            <a href="<?=twitter_link();?>" class="btn-social twitter" target="_blank" data-log-ga
+            data-ga-category="twitter-button"
+            data-ga-action="click"
+            data-ga-label="Twitter"
+            data-fb-event="CompleteAction"
+            data-fb-event-name="Twitter"
+            data-link-out="<?=twitter_link();?>"><i></i> Twitter</a>
+
+          </div>
+
+          <div>
+          <div class="story-paragraph">
+            <?= $fields['story_heading']; ?>
+            <?= $fields['story_content']; ?>
+          </div>
+          <div class="norfolk-paragraph">
+            <?= $fields['norfolk_heading']; ?>
+            <?= $fields['norfolk_content']; ?>
+          </div>
+          </div>
+
+
           <?php the_content();
           if(false){ ?>
             <p><span class="intro">UMMA MILLI’S STORY</span> The UN has called the crisis a textbook example of ethnic cleansing. Umma is 8 years old and is currently living in Balukhali refugee camp in Bangladesh.</p>
             <p>Water is a major challenge for those living in the camps. With campsites prone to floods and overcrowding, disease outbreaks are common. A shortage of toilets poses a further risk to water borne diseases. Many children like Umma are having to bathe in dirty ponds.</p>
           <?php } ?>
+        </div>
+
+
+        <div class="highlighted">
+          <?= $fields['highlight_section']; ?>
         </div>
 
         <div class="for-cta">
