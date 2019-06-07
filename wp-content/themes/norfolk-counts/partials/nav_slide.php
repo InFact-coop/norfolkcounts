eba3153b34e6b4a3004ed996aa2cd74c1cc6c926
lucymk <lucy.m.king92@gmail.com>
2019-06-07 15:21:45 +0100

    Removed comment from nav-slide.php

--- wp-content/themes/norfolk-counts/partials/nav_slide.php
+++ wp-content/themes/norfolk-counts/partials/nav_slide.php
@@ -1,9 +1,2 @@
-<?=
-/*
-*
-*
-* Expects $i to be supplied integer and $post to be a WP post object
-*
-*/
-?>
+
 <div class="slide"><a data-slide-go="<?= $i ?>"><i></i> <span><?= $post->post_title ?></span></a></div>
