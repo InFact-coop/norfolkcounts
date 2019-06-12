15f45633dd923410b9a202658388c0b85ee1331d
lucymk <lucy.m.king92@gmail.com>
2019-06-12 12:06:51 +0100

    Removes nav slider on mobile layout, adds banner to article view

--- wp-content/themes/norfolk-counts/partials/article-intro.php
+++ wp-content/themes/norfolk-counts/partials/article-intro.php
@@ -11,14 +11,7 @@ $fields = get_fields($post->ID);
 ?>
 
 <!-- Introduction -->
-<div
-  class="article-introduction"
-  data-ga-category="stories"
-  data-ga-action="view"
-  data-ga-label="<?= esc_attr($post->post_title) ?>"
-  data-fb-event="ViewContent"
-  data-fb-event-name="<?= esc_attr($post->post_title) ?>"
->
+<div class="article-introduction" data-ga-category="stories" data-ga-action="view" data-ga-label="<?= esc_attr($post->post_title) ?>" data-fb-event="ViewContent" data-fb-event-name="<?= esc_attr($post->post_title) ?>">
   <div class="row">
     <div class="small-12 medium-6 columns">
       <div class="article-content">
@@ -33,43 +26,38 @@ $fields = get_fields($post->ID);
             <div class="cta-heading">Ask Liz Truss to protect Norfolk's Aid contributions.</div>
             <div class="cta-buttons">
               <?php wp_reset_postdata(); ?>
-              <a 
-                href="<?= mailto_link() ?>" 
-                class="btn-social email" 
-                data-log-ga
-                data-ga-category="email-button"
-                data-ga-action="click"
-                data-ga-label="Email"
-                data-fb-event="CompleteAction"
-                data-fb-event-name="Email"
-                data-link-out="<?= mailto_link() ?>">Email
-              </a>
-              <a 
-                href="<?= twitter_link() ?>" 
-                class="btn-social twitter" 
-                target="_blank" 
-                data-log-ga
-                data-ga-category="twitter-button"
-                data-ga-action="click"
-                data-ga-label="Twitter"
-                data-fb-event="CompleteAction"
-                data-fb-event-name="Twitter"
-                data-link-out="<?= twitter_link() ?>">Twitter
+              <button data-open="doGooderModal" type="button" name="button" data-log-ga data-ga-category="email-button" data-ga-action="click" data-ga-label="Email" data-fb-event="CompleteAction" data-fb-event-name="Email" class="social active begin pointer email"><i></i> Email
+              </button>
+
+              <a href="https://ctt.ac/2ffE2" target="_blank" data-log-ga data-ga-category="tweet-mp-button" data-ga-action="click" data-ga-label="Tweet MP" data-fb-event="CompleteAction" data-fb-event-name="Tweet MP" data-link-out="https://ctt.ac/2ffE2">
+                <button type="button" name="button" class="social active begin pointer twitter"><i></i>
+                  Tweet
+                </button>
+
               </a>
             </div>
           </div>
 
+          <div class="small-12 medium-6 columns display-small-screens-only">
+            <figure class="article-image">
+              <img src="<?= get_featured_image_src() ?>" alt="<?php esc_attr_e($post->post_title); ?>" />
+              <figcaption>
+                Credit:
+                <?= get_featured_image_caption() ?>
+              </figcaption>
+            </figure>
+          </div>
+
+
           <div>
-          <div class="story-paragraph">
-            <div class="paragraph-heading"><?= $fields['story_heading'] ?></div>
-            <?= $fields['story_content'] ?>
-          </div>
-          <div class="norfolk-paragraph">
-            <div class="paragraph-heading"><?= $fields[
-              'norfolk_heading'
-            ] ?> </div>
-            <?= $fields['norfolk_content'] ?>
-          </div>
+            <div class="story-paragraph">
+              <div class="paragraph-heading"><?= $fields['story_heading'] ?></div>
+              <?= $fields['story_content'] ?>
+            </div>
+            <div class="norfolk-paragraph">
+              <div class="paragraph-heading"><?= $fields['norfolk_heading'] ?> </div>
+              <?= $fields['norfolk_content'] ?>
+            </div>
           </div>
 
 
@@ -88,43 +76,23 @@ $fields = get_fields($post->ID);
         </div>
 
         <div>
-        <div class="for-cta">
-          <?php $frontpage_id = get_option('page_on_front'); ?>
-          <span class="tag"><?= $post->post_title ?></span>
-          <span class="title"
-            ><?= get_field('scroll_cta_text', $frontpage_id) ?></span
-          >
-        </div>
-      </div>
-
-      <button
-        data-open="doGooderModal"
-        type="button"
-        name="button"
-        data-log-ga
-        data-ga-category="email-button"
-        data-ga-action="click"
-        data-ga-label="Email"
-        data-fb-event="CompleteAction"
-        data-fb-event-name="Email"
-        class="btn-action active begin pointer email"
-      ><i></i> Email
-      </button>
-
-      <button
-        type="button"
-        name="button"
-        class="btn-action active begin pointer twitter"
-      ><i></i> Tweet
-      </button>
-    </div>
+          <div class="for-cta">
+            <?php $frontpage_id = get_option('page_on_front'); ?>
+            <span class="tag"><?= $post->post_title ?></span>
+            <span class="title"><?= get_field('scroll_cta_text', $frontpage_id) ?></span>
           </div>
-    <div class="small-12 medium-6 columns">
+        </div>
+        <!-- 
+          <button data-open="doGooderModal" type="button" name="button" data-log-ga data-ga-category="email-button" data-ga-action="click" data-ga-label="Email" data-fb-event="CompleteAction" data-fb-event-name="Email" class="btn-action active begin pointer email"><i></i> Email
+          </button>
+
+          <button type="button" name="button" class="btn-action active begin pointer twitter"><i></i> Tweet
+          </button> -->
+      </div>
+    </div>
+    <div class="small-12 medium-6 columns display-large-screens-only">
       <figure class="article-image">
-        <img
-          src="<?= get_featured_image_src() ?>"
-          alt="<?php esc_attr_e($post->post_title); ?>"
-        />
+        <img src="<?= get_featured_image_src() ?>" alt="<?php esc_attr_e($post->post_title); ?>" />
         <figcaption>
           Credit:
           <?= get_featured_image_caption() ?>
@@ -133,4 +101,30 @@ $fields = get_fields($post->ID);
     </div>
   </div>
 </div>
+<div class="bottom-buttons">
+  <?php wp_reset_postdata(); ?>
+  <button data-open="doGooderModal" type="button" name="button" data-log-ga data-ga-category="email-button" data-ga-action="click" data-ga-label="Email" data-fb-event="CompleteAction" data-fb-event-name="Email" class="social active begin pointer email"><i></i> Email
+  </button>
+
+  <a href="https://ctt.ac/2ffE2" target="_blank" data-log-ga data-ga-category="tweet-mp-button" data-ga-action="click" data-ga-label="Tweet MP" data-fb-event="CompleteAction" data-fb-event-name="Tweet MP" data-link-out="https://ctt.ac/2ffE2">
+    <button type="button" name="button" class="social active begin pointer twitter"><i></i>
+      Tweet
+    </button>
+  </a>
+</div>
+
+<div class="bottom-banner">
+  <p class="banner-heading">Tell Liz Truss that Norfolk is against these devastating cuts.<p>
+      <p class="banner-content">Let her know why you’re proud of Norfolk's contribution to UK aid and why you want it protected.</p>
+      <div class="button-banner-container">
+        <button data-open="doGooderModal" type="button" data-ga-label="Email" data-fb-event="CompleteAction" data-fb-event-name="Email" class="social active begin pointer email"><i></i> Email
+        </button>
+        <a href="https://ctt.ac/2ffE2" target="_blank" data-log-ga data-ga-category="tweet-mp-button" data-ga-action="click" data-ga-label="Tweet MP" data-fb-event="CompleteAction" data-fb-event-name="Tweet MP" data-link-out="https://ctt.ac/2ffE2">
+          <button type="button" name="button" class="social active begin pointer twitter"><i></i>
+            Tweet
+          </button>
+        </a>
+      </div>
+</div>
+
 <!-- //Introduction -->
