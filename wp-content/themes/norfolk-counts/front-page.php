2bace15fc2069f5bbb271420c037dd40a7de304e
lucymk <lucy.m.king92@gmail.com>
2019-06-20 12:18:50 +0100

    shuffles order of stories relates #42

--- wp-content/themes/norfolk-counts/front-page.php
+++ wp-content/themes/norfolk-counts/front-page.php
@@ -1,36 +1,19 @@
 <?php the_post();
 $fields = get_fields();
 $stories = get_all_stories();
+shuffle($stories);
 ?>
 <!-- STEPS CONTENT -->
 <section class="steps-container">
   <!-- Step #0 -->
-  <div 
-  class="step step-0"
-  data-log-ga
-  data-ga-category="view-tool"
-  data-ga-action="view"
-  data-ga-label="View landing page of tool">
+  <div class="step step-0" data-log-ga data-ga-category="view-tool" data-ga-action="view" data-ga-label="View landing page of tool">
     <div class="row">
       <div class="small-12 columns text-center">
-        <img
-          src="<?php echo get_stylesheet_directory_uri(); ?>/images/img-windmill.svg"
-          alt=""
-          class="img-house"
-        />
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/img-windmill.svg" alt="" class="img-house" />
         <h1><?= get_bloginfo('name') ?></h1>
         <?php the_content(); ?>
 
-        <a
-          href="#"
-          data-log-ga
-          data-ga-category="find-out-button"
-          data-ga-action="click"
-          data-ga-label="Find out what"
-          class="btn-action active uppercase begin"
-          data-go-step="1"
-          ><?= $fields['step_1_cta'] ?></a
-        >
+        <a href="#" data-log-ga data-ga-category="find-out-button" data-ga-action="click" data-ga-label="Find out what" class="btn-action active uppercase begin" data-go-step="1"><?= $fields['step_1_cta'] ?></a>
       </div>
     </div>
   </div>
@@ -45,26 +28,8 @@ $stories = get_all_stories();
         <h2><?= $fields['step_1']['step_1_question'] ?></h2>
         <p><?= $fields['step_1']['step_1_text'] ?></p>
 
-        <a
-          href="#"
-          class="btn-action light"
-          data-log-ga
-          data-ga-category="live-in-norfolk-button"
-          data-ga-action="click"
-          data-ga-label="Yes - Lives in Norfolk"
-          data-go-step="2"
-          >Yes, I do!</a
-        >
-        <a
-          href="#"
-          class="btn-action light"
-          data-log-ga
-          data-ga-category="not-in-norfolk-button"
-          data-ga-action="click"
-          data-ga-label="No - doesn't live in Norfolk"
-          data-go-step="1-no"
-          >No, I don't...</a
-        >
+        <a href="#" class="btn-action light" data-log-ga data-ga-category="live-in-norfolk-button" data-ga-action="click" data-ga-label="Yes - Lives in Norfolk" data-go-step="2">Yes, I do!</a>
+        <a href="#" class="btn-action light" data-log-ga data-ga-category="not-in-norfolk-button" data-ga-action="click" data-ga-label="No - doesn't live in Norfolk" data-go-step="1-no">No, I don't...</a>
       </div>
     </div>
   </div>
@@ -78,28 +43,10 @@ $stories = get_all_stories();
 
         <h2><?= $fields['step_1']['no_answer_text'] ?></h2>
         <p>
-          <a
-            href="<?= $fields['step_1']['no_answer_link']['url'] ?>"
-            data-log-ga
-            data-ga-category="metro-link"
-            data-ga-action="click"
-            data-ga-label="Clicked 'reasons to move'"
-            data-link-out="<?= $fields['step_1']['no_answer_link']['url'] ?>"
-            target="_blank"
-            ><?= $fields['step_1']['no_answer_link']['title'] ?></a
-          ><br />And, if you need yet another reason...
+          <a href="<?= $fields['step_1']['no_answer_link']['url'] ?>" data-log-ga data-ga-category="metro-link" data-ga-action="click" data-ga-label="Clicked 'reasons to move'" data-link-out="<?= $fields['step_1']['no_answer_link']['url'] ?>" target="_blank"><?= $fields['step_1']['no_answer_link']['title'] ?></a><br />And, if you need yet another reason...
         </p>
 
-        <a
-          href="#"
-          class="btn-action light large"
-          data-log-ga
-          data-ga-category="norfolk-is-proud"
-          data-ga-action="click"
-          data-ga-label="Clicked 'why norfolk is proud'"
-          data-go-step="2"
-          ><?= $fields['step_1']['no_answer_cta'] ?></a
-        >
+        <a href="#" class="btn-action light large" data-log-ga data-ga-category="norfolk-is-proud" data-ga-action="click" data-ga-label="Clicked 'why norfolk is proud'" data-go-step="2"><?= $fields['step_1']['no_answer_cta'] ?></a>
       </div>
     </div>
   </div>
@@ -115,22 +62,10 @@ $stories = get_all_stories();
         <p><?= $fields['step_2']['step_2_sub_text'] ?></p>
 
         <ul class="option-list">
-          <?php foreach ($stories as $post): ?>
-          <li>
-            <a
-              href="#"
-              data-log-ga
-              data-ga-category="stories"
-              data-ga-action="view"
-              data-ga-label="<?= esc_attr($post->post_title) ?>"
-              data-fb-event="ViewContent"
-              data-fb-event-name="<?= esc_attr($post->post_title) ?>"
-              class="btn-action"
-              data-go-step="transition"
-              data-first-story="<?= $post->ID ?>"
-              ><?= $post->post_title ?></a
-            >
-          </li>
+          <?php foreach ($stories as $post) : ?>
+            <li>
+              <a href="#" data-log-ga data-ga-category="stories" data-ga-action="view" data-ga-label="<?= esc_attr($post->post_title) ?>" data-fb-event="ViewContent" data-fb-event-name="<?= esc_attr($post->post_title) ?>" class="btn-action" data-go-step="transition" data-first-story="<?= $post->ID ?>"><?= $post->post_title ?></a>
+            </li>
           <?php endforeach; ?>
         </ul>
       </div>
@@ -147,8 +82,8 @@ $stories = get_all_stories();
       <div class="small-12 columns text-center">
         <div class="rotator-icon"></div>
         <div class="messages">
-          <?php foreach ($fields['preloader'] as $row): ?>
-          <p><?= $row['loader_text'] ?></p>
+          <?php foreach ($fields['preloader'] as $row) : ?>
+            <p><?= $row['loader_text'] ?></p>
           <?php endforeach; ?>
         </div>
       </div>
